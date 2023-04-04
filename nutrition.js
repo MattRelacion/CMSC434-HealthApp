8adbd75e4acc55d2cb080cf10c94d7b9685cffe5
Reana Mahbub <rmahbub@umd.edu>
2023-04-04 19:00:48 -0400

    Fixed the text boxes mostly

--- nutrition.js
+++ nutrition.js
@@ -1,39 +1,32 @@
 const addButton = document.querySelector('.add-button');
-const box = document.querySelector('.box');
+const container = document.querySelector('.container');
 
 addButton.addEventListener('click', () => {
-  const newInputsWrapper = document.createElement('div');
-  newInputsWrapper.classList.add('inputs-wrapper');
+  const row = document.createElement('div');
+  row.classList.add('row');
   
   const foodNameInput = document.createElement('input');
   foodNameInput.type = 'text';
   foodNameInput.placeholder = 'Food Name';
-  foodNameInput.id = 'foodName';
   foodNameInput.name = 'foodName';
-  foodNameInput.classList.add('food-name-input');
-  
+
   const servingsInput = document.createElement('input');
   servingsInput.type = 'text';
   servingsInput.placeholder = 'Servings';
-  servingsInput.id = 'servings';
   servingsInput.name = 'servings';
-  servingsInput.classList.add('servings-input');
-  
+
   const equalsSign = document.createElement('span');
-  equalsSign.classList.add('equal-sign');
-  equalsSign.innerText = '=';
-  
+  equalsSign.textContent = '=';
+
   const caloriesInput = document.createElement('input');
   caloriesInput.type = 'text';
   caloriesInput.placeholder = 'Calories';
-  caloriesInput.id = 'calories';
   caloriesInput.name = 'calories';
-  caloriesInput.classList.add('calories-input');
-  
-  newInputsWrapper.appendChild(foodNameInput);
-  newInputsWrapper.appendChild(servingsInput);
-  newInputsWrapper.appendChild(equalsSign);
-  newInputsWrapper.appendChild(caloriesInput);
-  
-  box.appendChild(newInputsWrapper);
+
+  row.appendChild(foodNameInput);
+  row.appendChild(servingsInput);
+  row.appendChild(equalsSign);
+  row.appendChild(caloriesInput);
+
+  container.appendChild(row);
 });
